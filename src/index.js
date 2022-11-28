8896f02eeeac92c17e9041642cf9d9fcebd60f94
RossannaAriza <rossanna.ariza@gmail.com>
2022-11-28 13:29:18 -0500

    se crea estructura html se empieza a colocar funcionalidad a botones sin exito, se genera con ayuda de CSS diseño de como queremos que se vea la pagina

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+function continuar(){
+    PosActual = 0;
+    
+    document.getElementById("bienvenida").style.display="none";
+    document.getElementById("introduccion").style.display="block";
+    document.getElementById("instrucciones").style.display="none";
+    document.getElementById("encriptar").style.display="none";
+    document.getElementById("decifrar").style.display="none";
+    
+    }
+
