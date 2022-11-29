ef43a54d1ac2456829a9aa8f55d9b5a4b8188f86
RossannaAriza <rossanna.ariza@gmail.com>
2022-11-29 11:51:02 -0500

    se hacen modificaciones por error en el funcionamiento de js, se installa npm

--- src/cipher.js
+++ src/cipher.js
@@ -3,14 +3,4 @@ const cipher = {
 };
 
 export default cipher;
-function continuarintroduccion(){
-  PosActual = 0;
-  
-  document.getElementById("bienvenida").style.display="none";
-  document.getElementById("introduccion").style.display="block";
-  document.getElementById("instrucciones").style.display="none";
-  document.getElementById("encriptar").style.display="none";
-  document.getElementById("decifrar").style.display="none";
-  
-  }
-  
+
--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@ import cipher from './cipher.js';
 
 console.log(cipher);
 
-function continuar(){
-    PosActual = 0;
-    
-    document.getElementById("bienvenida").style.display="none";
-    document.getElementById("introduccion").style.display="block";
-    document.getElementById("instrucciones").style.display="none";
-    document.getElementById("encriptar").style.display="none";
-    document.getElementById("decifrar").style.display="none";
-    
-    }
 
+
+function continuar(){
+    posActual = 0;
+
+document.getElementById("bienvenida").style.display="none";
+document.getElementById("introduccion").style.display="block";
+document.getElementById("instrucciones").style.display="none";
+document.getElementById("encriptar").style.display="none";
+document.getElementById("decifrar").style.display="none";
+
+let nombre=document.getElementById("alias").value;
+  alert("Bienvenida " + nombre + " ¡Empecemos!");
+
+
+}
