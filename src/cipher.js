acb263b3b4a8bb162e09947292723d60616de16c
RossannaAriza <rossanna.ariza@gmail.com>
2022-12-02 11:07:22 -0500

    se generan cambios en diseño y se agregan unas funcionalidades en botones se empieza a generar el codigo de javascript primero declarando variables y se dejan otros codigos como comentarios por el momento

--- src/cipher.js
+++ src/cipher.js
@@ -1,2 +1,19 @@
+const cipher = {
+  // ...
+};
+
+export default cipher;
 
 
+const alfabeto = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+//const textoEncriptado = document.getElementById("textoEncriptado");
+//const encriptado = document.getElementById("encriptado");
+//const resultadoEncriptado = document.getElementById("resultadoEncriptado");
+//const posicionEncriptado = document.getElementById("posicionEncriptado");
+
+
+
+//se declara variables para tomar el texto encriptado y guardarlo en la consola
+//se establecen parametros de posicion
+//se encripta el texto y se devuelve en la consola
+//se asignan botones y funcionalidades para devolver el texto encriptado
--- src/index.js
+++ src/index.js
@@ -2,6 +2,32 @@ import cipher from './cipher.js';
 
 console.log(cipher);
 
-function inicio(){
-  alert("Bienvenida, ¡Empecemos!");
-}
+let name = document.querySelector(".name");
+
+console.log(name);
+
+// se declaran variable para hacer funcional las div en el momento que el usuario lo visualiza
+//const bloqueBienvenida = document.querySelector("#bienvenida");
+
+//const bloqueIntroduccion = document.querySelector("#introduccion");
+
+//const bloqueInstrucciones = document.querySelector("#instrucciones");
+
+//const bloqueEncriptar = document.querySelector("#encriptar");
+
+//const bloqueDecifrar = document.querySelector("#decifrar");
+
+// se declara los botones para desplazarse en la estructura de la pagina y se le asigna las sentencias
+//const botonIntroduccion = document.querySelector("button");
+
+//botonIntroduccion.addEventListener("click",function(){
+//bloqueBienvenida.style.display="none";
+//bloqueIntroduccion.style.display='block';
+//bloqueInstrucciones.style.display='none';
+//bloqueEncriptar.style.display='none';
+//bloqueDecifrar.style.display='none';
+// crear un alert a este boton que salude con el nombre ingresado
+
+//});
+
+// asignar botones de volver a inicio
